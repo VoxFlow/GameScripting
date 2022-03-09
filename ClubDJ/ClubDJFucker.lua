9f738cffb0d33f6d2f5cfa2e5ec1e9b5f5132e46
VoxFlow <66428350+VoxFlow@users.noreply.github.com>
2022-03-08 21:36:37 -0600

    Delete ClubDJ directory

--- ClubDJ/ClubDJFucker.lua
+++
@@ -1,7 +0,0 @@
---[[	Reddit link:https://www.reddit.com/r/robloxhackers/comments/m5m62q/release_club_dj_fucker_link_to_game_in_comments/
- 	Creator: probably u/XxxTheBrighterxxX on Reddit
-  Creator paste: https://paste.gg/p/anonymous/c29197fb39344ffd8d0133d37f7793d4	--]]
-
-loadstring(game:HttpGet("https://paste.gg/p/anonymous/c29197fb39344ffd8d0133d37f7793d4/files/b8a3da1cae83403ebe2b221900baf39a/raw", true))()
-
--- or loadstring(game:HttpGet("https://raw.githubusercontent.com/VoxFlow/GameScripting/main/ClubDJ/ClubDJFucker", true))()
